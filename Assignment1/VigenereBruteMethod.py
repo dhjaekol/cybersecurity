358ea1e1ac6ab0c3bc7d2c6e5da55fadf40ae848
mark <mthompson.dev@gmail.com>
2017-09-01 21:28:19 -0500

    Update

--- Assignment1/VigenereBruteMethod.py
+++ Assignment1/VigenereBruteMethod.py
@@ -1,18 +1,19 @@
 import collections
 
+
 class VigenereBruteMethod:
     _cipher = ""
 
     def __init__(self, cipherText):
         self._cipher = cipherText
 
-    def RunMethod(self):
+    @staticmethod
+    def runmethod():
         keywords = []
         with open("words.txt") as f:
             for line in f:
                 line = line.replace('\n','')
                 if len(line) == 5:
                     keywords.append(line)
-        print(len(keywords))
-        print(keywords)
-        
+
+        letterDict = {'A': 0, 'B': 1, 'C': 2, 'D' : 3, 'E': 4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, }
