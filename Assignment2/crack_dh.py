be304b44e317106d1305b70d421e9cf36910d46f
Mark Thompson <mar.thompson@cgifederal.com>
2017-09-20 07:19:58 -0500

    Keep going - harness your will

--- Assignment2/crack_dh.py
+++ Assignment2/crack_dh.py
@@ -10,17 +10,10 @@ def main():
     # --alice (A)
     # --bob (B)
 
-    parser = argparse.ArgumentParser(description="Diffie-Hellman")
-    parser.add_argument('g', metavar='g', type=int, nargs='+', help='generator')
-    parser.add_argument('n', metavar='n', type=int, nargs='+', help='modulus')
-    parser.add_argument('A', metavar='alice', type=int, nargs='+', help='alice')
-    parser.add_argument('B', metavar='bob', type=int, nargs='+', help='bob')
+    parser = buildParser()
     args = parser.parse_args()
-    g = args.g[0]
-    n = args.n[0]
-    A = args.A[0]
-    B = args.B[0]
-    print ("g: " + str(g) + ", n: " + str(n) + ", A: " + str(A) + ", B: " + str(B))
+    g, n, A, B = getParserArgs(args)
+    p = n
 
     # Alice sends A to Bob and Bob sends B to Alice
     # Alice computes shared key k = Ba mod p
@@ -30,15 +23,14 @@ def main():
     # p = 23
 
     # 2. Alice chooses a secret integer a whose value is 6 and computes
-    # A = g**a mod p =  5**6 mod 23 = 8
+    # A = g**a mod p
+    #   = 5**6 mod 23
+    #   = 8
     # a = 6
     # A = (g**a) % p
     # print("A: " + str(A))
 
-    ecgdA = egcd(A, B)
-    print(ecgdA)
-
-    modInv()
+    a = modInv(g,p)
 
     # 3. Bob chooses a secret integer b whose value is 15 and computes
     # B = g**b % p = 5**15 % 23 = 19
@@ -61,23 +53,46 @@ def main():
     print ("Shared secret computed by Alice (B^a mod n): ")
     print ("Shared secret computed by Bob (A^b mod n):  ")
 
+
+# http://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+def modInv(a, b):
+    a = a % m
+    x = 1
+    while (x < m):
+        if (a * x) % m == 1:
+            return x
+        i = i + 1
+
+
 def egcd(a, b):
-    x,y, u,v = 0,1, 1,0
+    x, y, u, v = 0, 1, 1, 0
     while a != 0:
-        q, r = b//a, b%a
-        m, n = x-u*q, y-v*q
-        b,a, x,y, u,v = a,r, u,v, m,n
+        q, r = b // a, b % a
+        m, n = x - u * q, y - v * q
+        b, a, x, y, u, v = a, r, u, v, m, n
     gcd = a
     return gcd, x, y
 
-# http://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
-def modInv (a, b):
-    a = a%m
-    x = 1
-    while (x < m):
-        if (a*x) % m == 1:
-            return x
-        i = i + 1
+
+def getParserArgs(args):
+    g = args.g[0]
+    n = args.n[0]
+    A = args.A[0]
+    B = args.B[0]
+    # print ("g: " + str(g) + ", n: " + str(n) + ", A: " + str(A) + ", B: " + str(B))
+    return g, n, A, B
+
+
+def buildParser():
+    parser = argparse.ArgumentParser(description="Diffie-Hellman")
+    parser.add_argument('g', metavar='g', type=int, nargs='+', help='generator')
+    parser.add_argument('n', metavar='n', type=int, nargs='+', help='modulus')
+    parser.add_argument('A', metavar='alice', type=int, nargs='+', help='alice')
+    parser.add_argument('B', metavar='bob', type=int, nargs='+', help='bob')
+    return parser
+
+
+
 
 if __name__ == "__main__":
     main()
