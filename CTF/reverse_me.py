37114392030d4346429575daa55cba1baee74b10
Mark Thompson <mthompson.dev@gmail.com>
2017-11-23 19:01:51 -0600

    Finished CTF reverse engineering. Started Assignment 7

--- CTF/reverse_me.py
+++ CTF/reverse_me.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python2
 import string
 
-
 def xor(x):
     return ''.join(chr(ord(i) ^ 10) for i in x)
-
 
 def shift(x):
     upper = string.ascii_uppercase
@@ -22,10 +20,27 @@ def shift(x):
             n.append(i)
     return n
 
+def reverseshift(x):
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    digits = string.digits
+    n = []
+    for i in x:
+        if i.isupper() is True:
+            n.append(upper[(upper.index(i) - 3) % 26])
+        elif i.islower() is True:
+            n.append(lower[(lower.index(i) - 3) % 26])
+        elif i.isdigit() is True:
+            n.append(digits[(digits.index(i) - 3) % 10])
+        else:
+            n.append(i)
+    return n
 
 def encode(x):
-    return x.encode("hex")    
+    return x.encode("hex")
 
+def hexdecode(x):
+    return x.decode("hex")
 
 if __name__ == "__main__":
     code = "63656e60716c7f687d39557f3c733d7f7c667b60556c616e6565627b606277"
@@ -36,6 +51,14 @@ if __name__ == "__main__":
     arr = raw_input()
 
     your_code = encode(xor(shift(arr)))
+
+    reverse_string = reverseshift(xor(hexdecode(code)))
+    print(reverse_string)
+
+    #foo = xor('a')
+    #print(foo)
+    #foo2 = xor(foo)
+    #print(foo2)
 
     if your_code == code:
         print "Yeah!....You are a Genius"
